eca0ad67827496ee2f299195970a94877b94096f
ruslana_hural <rusjah1@gmail.com>
2023-11-13 02:12:23 +0100

    added function displayProjects which created cards with projects and add display it on page

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import data from './data.json' assert {type : 'json'};
 
 const skillWrap = document.getElementById('skills-wrap');
+const projects = document.getElementById('pr-list');
+const prevProjBtn = document.getElementById('prev');
+const nextProjBtn = document.getElementById('next');
 
 console.log(skillWrap);
 
-function createSkils() {
+function displaySkils() {
 
     skillWrap.innerHTML = '';
 
@@ -26,8 +29,59 @@ function createSkils() {
 
         skillBorder.appendChild(skill);
         skillWrap.appendChild(skillBorder);
-        console.log(i, val);
     }
 }
 
-createSkils();
+function displayProjects() {
+    data.projects.forEach((i, ind) => {
+        const project = document.createElement('div');
+        const projectImg = document.createElement('div');
+        const projectName = document.createElement('p');
+        const projectDetail = document.createElement('a');
+        const usedLanguages = document.createElement('div');
+
+        let switcher = '';
+
+        if ((ind + 1) % 3 == 0) {
+            switcher = "pr-right";
+        } else if ((ind + 1) % 2 == 0) {
+            switcher = "pr-left";
+        }  else {
+            switcher = "pr-center";
+        }
+
+
+        project.classList.add("project", switcher);
+        projectImg.className = "project-img";
+        projectName.className = "pr--name";
+        projectDetail.className = "pr-more";
+        usedLanguages.className = "prog-languages";
+
+        projectDetail.target = "_blank";
+        projectDetail.href = i.link;
+        projectDetail.textContent = "More..."
+        projectName.textContent = i.name;
+        projectImg.style.backgroundImage = i.photo;
+
+        projectImg.append(projectName, projectDetail);
+        
+        for (let j of i.progLang) {
+            const usedLanguage = document.createElement('div');
+            const prjCircle = document.createElement('span');
+            const prjLanContent = document.createElement('span');
+
+            usedLanguage.className = "prog-language";
+            prjCircle.className = "pr-cir";
+            prjLanContent.textContent = j;
+
+            usedLanguage.append(prjCircle, prjLanContent);
+            usedLanguages.appendChild(usedLanguage);
+        }
+
+        project.append(projectImg, usedLanguages);
+        projects.appendChild(project);
+    })
+}
+
+displayProjects()
+displaySkils();
